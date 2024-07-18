605aadb8d779554f32b73c8ffe8ad8d32ea1c952
Amr Gamal <viscod01@gmail.com>
2024-07-18 20:55:48 +0300

    create super user

--- app/core/models.py
+++ app/core/models.py
@@ -8,10 +8,19 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must enter an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_super_user = True
+        user.save(using=self._db)
         return user
 
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,13 @@ class ModelTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'test1234')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test1234')
+
+    def test_create_super_user(self):
+        user = (get_user_model().objects
+                .create_superuser('test@example.com', 'test1234'))
+        self.assertTrue(user.is_super_user)
+        self.assertTrue(user.is_staff)
