46a7e5d57e3ab5029356329c58051603e95b1237
Amr Gamal <viscod01@gmail.com>
2024-07-19 21:49:47 +0300

    refactor: move user/admin related logic to user module

--- app/user/tests/test_admin.py
+++ app/user/tests/test_admin.py
@@ -17,20 +17,20 @@ class AdminSiteTests(TestCase):
         )
 
     def test_users_list(self):
-        url = reverse('admin:core_user_changelist')
+        url = reverse('admin:user_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
     def test_edit_user_page(self):
-        url = reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse('admin:user_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
-        url = reverse('admin:core_user_add')
+        url = reverse('admin:user_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
